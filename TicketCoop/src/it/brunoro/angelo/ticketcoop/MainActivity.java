0b663a3b50dd351a7a4f7dd2ffa3c051ebd5e4fb
Mattia Peterle <mattia.peterle@itecons.it>
2014-03-20 17:37:05 +0100

    Add code for open an Excel with external app

--- TicketCoop/src/it/brunoro/angelo/ticketcoop/MainActivity.java
+++ TicketCoop/src/it/brunoro/angelo/ticketcoop/MainActivity.java
@@ -1,7 +1,11 @@
 package it.brunoro.angelo.ticketcoop;
 
+import java.io.File;
+
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -18,6 +22,21 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    @Override
+    protected void onStart(){
+    	// Do something
+    	
+    	/*///code for open an Excel with external app
+    	Intent intent = new Intent(Intent.ACTION_VIEW);
+    	File file = new File("/sdcard/download/prova.xlsx");
+    	intent.setDataAndType(Uri.fromFile(file),"application/vnd.ms-excel");
+    	startActivity(intent); 
+    	*/
+    	
+    	// Recall parent
+    	super.onStart();
     }
     
 }
